26df0d3d698ed7d95f657bac8c4a40fd550b33b3
ethanhroe <ethan.h.roe@gmail.com>
2025-10-17 08:48:08 -0500

    Added defs for height, width and aliasing

--- src/main.cc
+++ src/main.cc
@@ -1,30 +1,39 @@
 #include "../include/vec3.h"
 #include "../include/color.h"
-#include "../include/ray.h"
+// #include "../include/ray.h"
 
 #include <stdio.h>
 
+#define WIDTH 1024
+#define HEIGHT 1024
+
+#define ALIASING 0
+
+#define SAMPLES_PER_PIXEL 4
+
 int main(int argc, char *argv[]){
 
-    int i,j;
+    int i,j,s;
     double r,g,b;
     FILE *f;
 
-    // Image
-    int image_width = 256;
-    int image_height = 256;
-
     // Render
     f = fopen("test_image.ppm", "w");
-    fprintf(f, "P3\n%d\n%d\n255\n", image_width, image_height);
-    for(j = 0; j < image_height; j++){
-        fprintf(stderr, "\rScanlines remaining: %d ", image_height - j);
+    fprintf(f, "P3\n%d\n%d\n255\n", WIDTH, HEIGHT);
+    for(j = 0; j < HEIGHT; j++){
+        fprintf(stderr, "\rScanlines remaining: %d ", HEIGHT - j);
         fflush(stderr);  // make sure it prints immediately
-        for(i = 0; i < image_width; i++){
+        for(i = 0; i < WIDTH; i++){
+
+            if(ALIASING == 1){
+                for(s = 0; s < SAMPLES_PER_PIXEL; s++){
+
+                }
+            }
 
             color pixel_color = {
-                (double) i / (image_width-1),
-                (double) j / (image_height-1),
+                (double) i / (WIDTH-1),
+                (double) j / (HEIGHT-1),
                 0.0
             };
 
