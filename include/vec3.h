eb204a945c01c755b567ef45da7d59ea111105ff
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-03 17:32:20 -0600

    Added additional utility functions

--- include/vec3.h
+++ include/vec3.h
@@ -19,6 +19,30 @@ using point3 = vec3;
 
 // Utility Functions
 
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+    return (1/t) * v;
+}
+
 inline double dot(vec3 v, vec3 w){
    return v[0] * w[0] + v[1] * w[1] + v[2] * w[2];
 }
