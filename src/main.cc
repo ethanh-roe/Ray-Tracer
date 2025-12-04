f50bdf377f7db21009cf3cbe2ddd16e35a728754
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-03 20:53:26 -0600

    Added max depth to limit ray bounces

--- src/main.cc
+++ src/main.cc
@@ -19,9 +19,10 @@ int main(int argc, char *argv[]){
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
-    cam.samples_per_pixel = 100;
+    cam.aspect_ratio        = 16.0 / 9.0;
+    cam.image_width         = 400;
+    cam.samples_per_pixel   = 100;
+    cam.max_depth           = 50;
 
     cam.render(world);
     return 0;
