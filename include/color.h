d88ea6acb25c02ae13f246a71d006ea112728fc9
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-03 20:15:54 -0600

    Changed write_color to use interval and clamp for determining color bytes

--- include/color.h
+++ include/color.h
@@ -11,14 +11,15 @@ void write_color(FILE *f, const color& pixel_color){
     double b = pixel_color.z();
 
     // translate the [0,1] component values ot the byte range [0,255]
-    int ir = (int)255.999 * r;
-    int ig = (int)255.999 * g;
-    int ib = (int)255.999 * b;
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     fprintf(f, "%d %d %d\n",
-                (unsigned char) (ir),
-                (unsigned char) (ig),
-                (unsigned char) (ib));
+                (unsigned char) (rbyte),
+                (unsigned char) (gbyte),
+                (unsigned char) (bbyte));
 }
 
 #endif
