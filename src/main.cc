ee4812372e812441bcea96fb75b5d8853df001b2
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-03 20:07:18 -0600

    Added camera class and refactored

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
 #include "../include/common_consts.h"
+
+#include "../include/camera.h"
 #include "../include/hittable.h"
 #include "../include/hittable_list.h"
 #include "../include/sphere.h"
@@ -9,72 +11,19 @@
 
 #define SAMPLES_PER_PIXEL 4
 
-color ray_color(const ray& r, const hittable& world){
-    // If we hit the sphere color it red
-    hit_record rec;
-    if(world.hit(r, interval(0, infinity), rec)){
-        return 0.5 * (rec.normal + color(1,1,1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
 int main(int argc, char *argv[]){
-
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-    // Calculate the image height
-    int image_height = int(image_width / aspect_ratio);
-    // Ensure it is atleast 1
-    image_height = (image_height < 1) ? 1 : image_height;
 
     // World
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0,0,0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the veritcal viewport edges
-    auto viewport_u = vec3(viewport_width,0,0);
-    auto viewport_v = vec3(0,-viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upperr left pixel
-    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    double r,g,b;
-    FILE *f;
-
-    // Render
-    f = fopen("test_image.ppm", "w");
-    fprintf(f, "P3\n%d\n%d\n255\n", image_width, image_height);
-    for(int j = 0; j < image_height; j++){
-        fprintf(stderr, "\rScanlines remaining: %d ", image_height - j);
-        fflush(stderr);  // make sure it prints immediately
-        for(int i = 0; i < image_width; i++){
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(f, pixel_color);
-        }
-    }
-    fprintf(stderr, "\rDone.                 \n");
-    fclose(f);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 400;
+    
+    cam.render(world);
     return 0;
 }
