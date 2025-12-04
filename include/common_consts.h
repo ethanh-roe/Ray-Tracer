a36d5c73e614541bf93b241f0d40f533bc72455d
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-03 19:25:25 -0600

    Added header for common utilities and constants

---
+++ include/common_consts.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_CONSTS_H
+#define COMMON_CONSTS_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+using namespace std;
+
+// Constants
+const double infinity = numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Functions
+inline double degrees_to_radians(double degrees){
+    return degrees * pi / 180.0;
+}
+
+// common headers
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
