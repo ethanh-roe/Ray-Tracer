79488644759840226d04bf94a397a5d951f841a4
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-08 15:54:38 -0600

    Added model for light scattering

--- include/vec3.h
+++ include/vec3.h
@@ -33,13 +33,20 @@ class vec3{
         return *this *= 1/t;
     }
 
-      double length() const {
+    double length() const {
          return std::sqrt(length_squared());
       }
 
       double length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) > s) && (fabs(e[2]) < s);
+    }
+
 
     static vec3 random(){
         return vec3(random_double(), random_double(), random_double());
