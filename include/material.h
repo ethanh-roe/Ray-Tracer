ad8395140d67384ab673dc16d45d0a2f581301f0
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-08 17:33:46 -0600

    Added ray time to scatter methods

--- include/material.h
+++ include/material.h
@@ -26,7 +26,7 @@ class lambertian : public material {
                 scatter_direction = rec.normal;
             }
 
-            scattered = ray(rec.p, scatter_direction);
+            scattered = ray(rec.p, scatter_direction, ray_in.time());
             attenuation = albedo;
             return true;
         }
@@ -42,7 +42,7 @@ class metal : public material {
         bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override{
             vec3 reflected = reflect(ray_in.direction(), rec.normal);
             reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected, ray_in.time());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
@@ -74,7 +74,7 @@ class dielectric : public material{
                 direction = refract(unit_direction, rec.normal, ri);
             }
 
-            scattered = ray(rec.p, direction);
+            scattered = ray(rec.p, direction, ray_in.time());
             return true;
         }
 
