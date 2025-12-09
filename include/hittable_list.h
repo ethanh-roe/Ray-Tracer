146af08dd27b6bb1dd82af34c9cd5789baeceaee
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-08 18:06:17 -0600

    Added bounding box to hittable list

--- include/hittable_list.h
+++ include/hittable_list.h
@@ -3,40 +3,47 @@
 
 #include <vector>
 
+#include "aabb.h"
 #include "hittable.h"
 
 class hittable_list : public hittable {
     public:
-    vector<shared_ptr<hittable>> objects;
+        vector<shared_ptr<hittable>> objects;
 
-    hittable_list(){}
-    hittable_list(shared_ptr<hittable> object) {
-        add(object);
-    }
-
-    void clear(){
-        objects.clear();
-    }
-
-    void add(shared_ptr<hittable> object){
-        objects.push_back(object);
-    }
-
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
-        hit_record temp_rec;
-        bool hit= false;
-        auto closest = ray_t.max;
-
-        for(const auto& object : objects){
-            if (object->hit(r, interval(ray_t.min, closest), temp_rec)){
-                hit = true;
-                closest = temp_rec.t;
-                rec = temp_rec;
-            }
+        hittable_list(){}
+        hittable_list(shared_ptr<hittable> object) {
+            add(object);
         }
 
-        return hit;
-    }
+        void clear(){
+            objects.clear();
+        }
+
+        void add(shared_ptr<hittable> object){
+            objects.push_back(object);
+            bbox = aabb(bbox, object->bounding_box());
+        }
+
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
+            hit_record temp_rec;
+            bool hit= false;
+            auto closest = ray_t.max;
+
+            for(const auto& object : objects){
+                if (object->hit(r, interval(ray_t.min, closest), temp_rec)){
+                    hit = true;
+                    closest = temp_rec.t;
+                    rec = temp_rec;
+                }
+            }
+
+            return hit;
+        }
+
+        aabb bounding_box() const override { return bbox; }
+
+    private:
+        aabb bbox;
 };
 
 #endif
