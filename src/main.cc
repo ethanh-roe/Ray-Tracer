34a67009894e779893dce151c15af2d0fb2e966a
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-08 19:18:58 -0600

    Made floor checkered texture for fancy scene

--- src/main.cc
+++ src/main.cc
@@ -6,14 +6,18 @@
 #include "../include/hittable_list.h"
 #include "../include/material.h"
 #include "../include/sphere.h"
+#include "../include/texture.h"
 
 #include <stdio.h>
 
 void fancy_scene(){
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
