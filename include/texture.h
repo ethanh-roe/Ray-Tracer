55394cbe2e5df3e55939dbf82e9f65f9d1d6d9f9
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-08 19:14:44 -0600

    Added checkered texture class

--- include/texture.h
+++ include/texture.h
@@ -1,7 +1,7 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-#include "color.h"
+#include "common_consts.h"
 
 class texture {
     public:
@@ -22,6 +22,30 @@ class solid_color : public texture {
 
     private:
         color albedo;
+};
+
+class checker_texture : public texture {
+    public:
+        checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd)
+            : inv_scale(1.0 / scale), even(even), odd(odd) {}
+
+        checker_texture(double scale, const color& c1, const color& c2) 
+            : checker_texture(scale, make_shared<solid_color>(c1), make_shared<solid_color>(c2)) {}
+
+        color value(double u, double v, const point3& p) const override {
+            auto xInt = int(floor(inv_scale * p.x()));
+            auto yInt = int(floor(inv_scale * p.y()));
+            auto zInt = int(floor(inv_scale * p.z()));
+            
+            bool isEven = (xInt + yInt + zInt) % 2 == 0;
+            
+            return isEven ? even->value(u, v, p) : odd->value(u, v, p);
+        }
+
+    private:
+        double inv_scale;
+        shared_ptr<texture> even;
+        shared_ptr<texture> odd;
 };
 
 #endif
