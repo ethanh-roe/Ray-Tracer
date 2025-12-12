dfa5f857480f5fb72203dfab5159eb881ff7c13c
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-12 12:36:56 -0600

    Added uniform hemispherical sampling

--- include/material.h
+++ include/material.h
@@ -29,7 +29,7 @@ class lambertian : public material {
         lambertian(shared_ptr<texture> tex) : tex(tex) {}
 
         bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override{
-            auto scatter_direction = rec.normal + random_unit_vector();
+            auto scatter_direction = random_on_hemisphere(rec.normal);
 
             // Catch degenerate scatter direction
             if(scatter_direction.near_zero()){
@@ -42,8 +42,7 @@ class lambertian : public material {
         }
 
         double scattering_pdf(const ray& r_in, const hit_record& rec, const ray& scattered) const override {
-            auto cos_theta = dot(rec.normal, unit_vector(scattered.direction()));
-            return cos_theta < 0 ? 0 : cos_theta / pi;
+            return 1 / (2 * pi);
         }
 
     private:
