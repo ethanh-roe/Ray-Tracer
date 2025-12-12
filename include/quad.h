07161b9e85612b58c5677f1acae6da3f96bbb566
ethanh-roe <ethan.h.roe@gmail.com>
2025-12-12 13:23:12 -0600

    Added pdf to hittable objects

--- include/quad.h
+++ include/quad.h
@@ -15,7 +15,7 @@ class quad : public hittable {
                 normal = unit_vector(n);
                 D = dot(normal, Q);
                 w = n / dot(n, n);
-
+                area = n.length();
                 set_bounding_box();
             }
 
@@ -64,6 +64,21 @@ class quad : public hittable {
             rec.v = b;
             return true;
         }
+
+        double pdf_value(const point3& origin, const vec3& direction) const override {
+            hit_record rec;
+            if(!this->hit(ray(origin, direction), interval(0.001, infinity), rec)) return 0;
+
+            auto distance_squared = rec.t * rec.t * direction.length_squared();
+            auto cosine = fabs(dot(direction, rec.normal) / direction.length());
+
+            return distance_squared / (cosine * area);
+        }
+
+        vec3 random(const point3& origin) const override {
+            auto p = Q + (random_double() * u) + (random_double() * v);
+            return p - origin;
+        }
     
     private:
         point3 Q;
@@ -73,6 +88,7 @@ class quad : public hittable {
         aabb bbox;
         vec3 normal;
         double D;
+        double area;
 };
 
 inline shared_ptr<hittable_list> box(const point3& a, const point3& b, shared_ptr<material> mat) {
